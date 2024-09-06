add2443cdf6ac6d346493ab51bfc7815e29d13e8
Rafael-BD <rafael8885326@gmail.com>
2024-09-05 22:39:46 -0300

    📦 NEW: Add and config Stitches

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
-import colors from '../style/colors';
+// import colors from '../style/colors';
+import { styled } from '../../stitches.config';
 
-const TopBar = styled.div`
-    background-color: ${colors.foreground};
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 80px;
-`;
+const Text = styled('p', {
+    fontFamily: '$system',
+    color: '$hiContrast',
+
+    variants: {
+        size: {
+            1: {
+                fontSize: '$1',
+            },
+            2: {
+                fontSize: '$2',
+            },
+            3: {
+                fontSize: '$3',
+            },
+        },
+    },
+});
 
 const HomePage = () => {
     return (
         <div>
-            <TopBar>
-                <h1>Top Bar</h1>
-            </TopBar>
+            <Text size="3">Bem-vindo à Home Page</Text>
 
             <h1>Bem-vindo à Home Page</h1>
-            <p>Esta é uma página simples em React.</p>
+            <p>Esta é uma página simples em NextJS.</p>
         </div>
     );
 };
